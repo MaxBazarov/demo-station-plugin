2a53a22fbecd7e96215c488a8f694b54458fc0ff
Max Bazarov <mbazarov@gmail.com>
2021-11-19 22:56:03 +0300

    Added exampes

---
+++ export-examples/registration/viewer/story.js
@@ -0,0 +1,189 @@
+var story = {
+ "docName": "Registration Demo",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "Maxim Bazarov",
+ "ownerEmail": "max@bazarov.ru",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": true,
+ "highlightLinks": true,
+ "pages": [
+  {
+   "id": "9:427",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Register",
+   "image": "register.png",
+   "image2x": "register@2x.png",
+   "index": 0,
+   "width": 1440,
+   "height": 1018,
+   "x": -1625,
+   "y": -1760,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 333,
+      "y": 25,
+      "width": 203,
+      "height": 14
+     },
+     "index": 0,
+     "url": "https://www.figma.com/community/file/1040896960754198585/Craftsystems.xyz-%2F%2F-Registration-flow"
+    },
+    {
+     "rect": {
+      "x": 649,
+      "y": 892,
+      "width": 102,
+      "height": 48
+     },
+     "index": 1,
+     "page": 2
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:347",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Forgot password",
+   "image": "forgot-password.png",
+   "image2x": "forgot-password@2x.png",
+   "index": 1,
+   "width": 1440,
+   "height": 1018,
+   "x": -785,
+   "y": 194,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 91,
+      "y": 765,
+      "width": 301,
+      "height": 48
+     },
+     "index": 2,
+     "page": 3
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:523",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Success register",
+   "image": "success-register.png",
+   "image2x": "success-register@2x.png",
+   "index": 2,
+   "width": 1440,
+   "height": 1018,
+   "x": 55,
+   "y": -1760,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 671,
+      "y": 587,
+      "width": 102,
+      "height": 48
+     },
+     "index": 3,
+     "page": 4
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:379",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Change password",
+   "image": "change-password.png",
+   "image2x": "change-password@2x.png",
+   "index": 3,
+   "width": 1440,
+   "height": 1018,
+   "x": 895,
+   "y": 194,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 91,
+      "y": 788,
+      "width": 301,
+      "height": 48
+     },
+     "index": 4,
+     "page": 4
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:300",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Log in",
+   "image": "log-in.png",
+   "image2x": "log-in@2x.png",
+   "index": 4,
+   "width": 1440,
+   "height": 1018,
+   "x": 1735,
+   "y": -1760,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 92,
+      "y": 518,
+      "width": 180,
+      "height": 33
+     },
+     "index": 5
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "9:299",
+   "name": "Main"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 5,
+ "backColor": "#B9BBB6"
+}
