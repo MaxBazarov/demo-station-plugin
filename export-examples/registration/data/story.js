dc46ee690e087ba7b1807706adfc31e99e28f812
MaxBazarov <mbazarov@gmail.com>
2022-12-11 22:47:06 +0300

    Examples republished

---
+++ export-examples/registration/data/story.js
@@ -0,0 +1,211 @@
+var story = {
+ "docName": "Registration Demo",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "serverToolsPath": "",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableInteractions": false,
+ "disableHotspots": false,
+ "hideGallery": false,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": true,
+ "highlightLinks": true,
+ "pages": [
+  {
+   "id": "9:427",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Register",
+   "image": "register.png",
+   "imageFull": "register.png",
+   "index": 0,
+   "width": 1440,
+   "height": 1018,
+   "x": -1625,
+   "y": -1760,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 333,
+      "y": 25,
+      "width": 203,
+      "height": 14
+     },
+     "index": 0,
+     "url": "https://www.figma.com/community/file/1040896960754198585/Craftsystems.xyz-%2F%2F-Registration-flow"
+    },
+    {
+     "rect": {
+      "x": 649,
+      "y": 892,
+      "width": 102,
+      "height": 48
+     },
+     "index": 1,
+     "page": 2
+    },
+    {
+     "rect": {
+      "x": 649,
+      "y": 892,
+      "width": 102,
+      "height": 48
+     },
+     "index": 2,
+     "page": 2
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:347",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Forgot password",
+   "image": "forgot-password.png",
+   "imageFull": "forgot-password.png",
+   "index": 1,
+   "width": 1440,
+   "height": 1018,
+   "x": -785,
+   "y": 194,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 91,
+      "y": 765,
+      "width": 301,
+      "height": 48
+     },
+     "index": 3,
+     "page": 3
+    },
+    {
+     "rect": {
+      "x": 91,
+      "y": 842,
+      "width": 301,
+      "height": 48
+     },
+     "index": 4,
+     "action": "back"
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:523",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Success register",
+   "image": "success-register.png",
+   "imageFull": "success-register.png",
+   "index": 2,
+   "width": 1440,
+   "height": 1018,
+   "x": 55,
+   "y": -1760,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 671,
+      "y": 587,
+      "width": 102,
+      "height": 48
+     },
+     "index": 5,
+     "page": 4
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:379",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Change password",
+   "image": "change-password.png",
+   "imageFull": "change-password.png",
+   "index": 3,
+   "width": 1440,
+   "height": 1018,
+   "x": 895,
+   "y": 194,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 91,
+      "y": 788,
+      "width": 301,
+      "height": 48
+     },
+     "index": 6,
+     "page": 4
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "9:300",
+   "groupID": "9:299",
+   "transAnimType": 0,
+   "title": "Log in",
+   "image": "log-in.png",
+   "imageFull": "log-in.png",
+   "index": 4,
+   "width": 1440,
+   "height": 1018,
+   "x": 1735,
+   "y": -1760,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 92,
+      "y": 518,
+      "width": 180,
+      "height": 33
+     },
+     "index": 7,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "9:299",
+   "name": "Main"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 5,
+ "backColor": "#FFFFFF"
+}
